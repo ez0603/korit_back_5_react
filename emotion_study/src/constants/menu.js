521a3105fc6fbea2e4d628a5fb10132e5650e3a2
ez0603 <dlwldjs3132@naver.com>
2024-02-28 17:51:57 +0900

    240228

--- emotion_study/src/constants/menu.js
+++ emotion_study/src/constants/menu.js
@@ -1,6 +1,9 @@
 import Mypage from "../pages/Mypage/Mypage";
 import ImageEx from "../pages/ImageEx/ImageEx"
 import ImageEx2 from "../pages/ImageEx2/ImageEx2";
+import ImageEx3 from "../pages/ImageEx3/ImageEx3";
+import BoardWrite from "../pages/BoardWrite/BoardWrite";
+import BoardList from "../pages/BoardList/BoardList";
 
 
 export const MENUS = [
@@ -33,5 +36,23 @@ export const MENUS = [
         path: "/image/ex2",   
         name: "다중 업로드",
         element: <ImageEx2 />
+    },
+    {
+        id: 6,
+        path: "/image/ex3",   
+        name: "이미지 여러개 불러오기",
+        element: <ImageEx3 />
+    },
+    {
+        id: 7,
+        path: "/board/write",   
+        name: "게시글 작성",
+        element: <BoardWrite />
+    },
+    {
+        id: 8,
+        path: "/board/list",   
+        name: "게시글 목록",
+        element: <BoardList />
     }
 ];
---
+++ emotion_study/src/hooks/inputHook.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export function useInput() {
+    const [ inputValue, setInputValue ] = useState("");
+
+    const onChange = (e) => {
+        const { value } = e.target;
+        setInputValue(() => value)
+      
+    }
+    return [ inputValue, onChange ];
+}
+
+export function useMaxSizeValidateInput(maxSize) {
+    const [ inputValue, setInputValue ] = useState("");
+
+    const onChange = (e) => {
+        const { value } = e.target;
+        if(value.length <= maxSize) {
+            setInputValue(() => value)
+        }
+      
+    }
+    return [ inputValue, onChange ]; // 비구조 할당
+}
+
